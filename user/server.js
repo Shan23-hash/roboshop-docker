804756946fd2788d0fa6737486f00eaef2809f8f
shanthi <shanbojja@gmail.com>
2025-07-19 00:01:30 +0530

    Docker

--- user/server.js
+++ user/server.js
@@ -277,4 +277,4 @@ mongoLoop();
 const port = process.env.USER_SERVER_PORT || '8080';
 app.listen(port, () => {
     logger.info('Started on port', port);
-});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+});
